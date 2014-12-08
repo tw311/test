78c758e2f9f180eaeab83b70868c311136cf24c6
Freelance Mate <tw311@yandex.com>
2014-12-08 11:21:28 +0200

    Initial commit

---
+++ app/database/migrations/2014_12_02_160237_create_test_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTestTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+        Schema::create('test', function($table)
+        {
+            $table->increments('id');
+            $table->string('title');
+            $table->timestamps();
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
